4478af4b1299c5877a854cd5fc06b78c18667101
hkari <hkari@jyu.fi>
2023-08-22 14:55:02 +0300

    Teht 3.15

--- index.js
+++ index.js
@@ -77,31 +77,18 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(500).end()
     })
 })
 
-/* app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-}) */
-
-/* const getRandomInt = (min, max) => {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    const id =  Math.floor(Math.random() * (max - min) + min)
-    const idExisting = persons.find(person => person.id === id)
-    if (idExisting !== undefined) {
-        console.log("Id must be unique")
-        return 0
-    }
-    return id
-} */
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -124,53 +111,14 @@ app.post('/api/persons', (request, response) => {
     })
   })
 
-/* app.post('/api/persons', (request, response) => {
-
-    const person = request.body
-    console.log(person)
-
-    let id = getRandomInt(1,100)
-    while (id === 0){
-        id = getRandomInt(1,100)
-    }
-
-    if ((!person.name) || (person.name.trim() === "")) {
-        return response.status(400).json({ 
-            error: 'name missing' 
-        })    
-    }
-
-    if ((!person.number) || (person.number.trim() === "")) {
-        return response.status(400).json({ 
-            error: 'number missing' 
-        })    
-    }
-
-    const names = persons.map(person => person.name)
-    if (names.includes(person.name)) {
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-        })    
-    }
-
-    const newPerson = {
-        id: id,
-        name: person.name,
-        number: person.number,
-    }
-
-    persons = persons.concat(newPerson)
-    console.log(newPerson)
-    response.json(newPerson)  
-}) */
-
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    console.log(persons)
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
 })
+
 
 app.get('/info', (req, res) => {
     const dataLength = persons.length
