531826ccb6f2ccf8748810bc1e7feef98c3a9723
hkari <hkari@jyu.fi>
2023-08-22 09:33:38 +0300

    Teht 3.9

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 //app.use(morgan('tiny'))
 
